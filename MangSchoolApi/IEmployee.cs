14e5b4a90eb68c055338d73136b86a2ce773cbd7
Taib-Ali-Alameri <90430348+Taib-Ali-Alameri@users.noreply.github.com>
2021-09-12 19:19:09 +0300

    Add files via upload

---
+++ MangSchoolApi/IEmployee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangSchoolApi
+{
+   public interface IEmployee
+    {
+        int Id { get; set; }
+
+        string FirstName { get; set; }
+        decimal Salary { get; set; }
+    }
+}
---
+++ MangSchoolApi/IEmployeeBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangSchoolApi
+{
+   public class IEmployeeBase:IEmployee
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public virtual decimal Salary { get; set; }
+    }
+}
